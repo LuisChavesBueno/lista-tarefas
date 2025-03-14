722abedbf8f5dbffc6a596aae013faf080727f6b
Luis Chaves <135845746+LuisChavesBueno@users.noreply.github.com>
2025-03-14 00:20:06 -0300

    carregar

--- index.js
+++ index.js
@@ -1,73 +1,16 @@
-const form = document.querySelector('.form');
-const tarefa = document.querySelector('.tarefa')
-const categoria = document.querySelector('.categoria')
-const listaTarefas = document.querySelector('.lista-tarefas')
+let valorTotal = 0;
 
 
+function comprar(produtoElement) {
+    const precoElement = produtoElement.querySelector(".preco");
+    const valorElement = document.getElementById("valor");
+    const precoTexto = precoElement.textContent;
+    const precoNumerico = parseInt(precoTexto);
 
-let id = 0
-
-const AtividadesAdicionadas  = () => {
-    const idAdicionados = document.querySelector('.id')
-    
-    id++
-
-    idAdicionados.innerHTML = id
-
+    if (!isNaN(precoNumerico)) {
+        valorTotal += precoNumerico;
+        valorElement.textContent = "Valor: R$"+ valorTotal+",00";
+    } else {
+        console.error("Preço inválido:", precoTexto);
+    }
 }
-
-const AdicionadoLista = () => {
-        
-    const div = document.createElement('div')
-    const p2 = document.createElement('p')
-    const p1 = document.createElement('p')
-    
-    listaTarefas.appendChild(div)
-    div.appendChild(p2)
-    div.appendChild(p1)
-
-    p1.classList.add('tarefa-escolida')
-    p2.classList.add('categoria-escolida')
-
-    div.style.display = 'block'
-
-    p1.innerHTML = tarefa.value
-    p2.innerHTML = categoria.value
-    AtividadesAdicionadas()
-}
-
-form.addEventListener('submit', (event) => {
-    event.preventDefault()
-    AdicionadoLista()
-});
-
-
-
-//categoria 
-
-const criarCategoria = document.querySelector('.criar-categoria')
-const digiteCategoriCriar = document.querySelector('.digite-categori-criar')
-const MessageCategoria = document.querySelector('.adicionar-tarefas  p')
-
-const TempoMessageCategoria = () => {
-    MessageCategoria.style.display = 'block'
-
-    setTimeout( () => {
-        MessageCategoria.style.display = 'none'
-    } , 2000)
-}
-
-const  criadoCategoria = () => {
-    const option = document.createElement('option')
-
-    categoria.appendChild(option)
-
-    option.innerHTML = digiteCategoriCriar.value
-}
-
-criarCategoria.addEventListener('submit' , (event) => {
-    event.preventDefault()
-    criadoCategoria()
-    TempoMessageCategoria()
-})
-
